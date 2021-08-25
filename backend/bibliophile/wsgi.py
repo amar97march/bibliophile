4e476b2212ea83d505e0a8e4dcfe68923160f6b8
Amar Singh <amar97march@gmail.com>
2021-08-25 16:17:37 +0530

    folder structure

--- backend/bibliophile/wsgi.py
+++ backend/bibliophile/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibliophile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.bibliophile.settings')
 
 application = get_wsgi_application()
